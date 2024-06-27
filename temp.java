5e88cac7b4bc0525aadb4c352f32e6c82c563c2d
Neutrino79 <atharvhiremath.ah@gmail.com>
2024-06-27 23:32:15 +0530

    Linked-List_AddDigit

---
+++ temp.java
@@ -0,0 +1,9 @@
+package Practice;
+
+public class temp {
+    public static void main(String[] args) {
+        int a=9;
+        int b=a/10;
+        System.out.println("\na="+a+"\nb="+b);
+    }
+}
